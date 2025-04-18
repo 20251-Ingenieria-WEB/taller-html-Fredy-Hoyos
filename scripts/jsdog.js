b8033fc6d01bb945a01ad793073d544f75f41103
unknown <fredy.hoyos1@udea.edu.co>
2025-04-18 13:44:30 -0500

    Se agrega nombre a perros

--- scripts/jsdog.js
+++ scripts/jsdog.js
@@ -14,16 +14,17 @@ function verImagenPerro() {
             return res.json();
         })
 
-        .then (data => mostrarImagenPerro(data, "dogInfo"))
+        .then (data => mostrarImagenPerro(data, "dogInfo", nombre))
         .catch(error => {
             document.getElementById("dogInfo").textContent = "Por favor ingresa un nombre de perro válido";
         });
 }
 
-function mostrarImagenPerro(data, contenedorId) {
+function mostrarImagenPerro(data, contenedorId, nombre) {
     const template = document.getElementById("perro-template").content.cloneNode(true);
 
     template.querySelector(".perro-img").src = data.message;
+    template.querySelector(".perro-nombre").textContent= nombre;
 
     const contenedor = document.getElementById(contenedorId);
     contenedor.innerHTML = ""; // Limpiar resultado anterior
